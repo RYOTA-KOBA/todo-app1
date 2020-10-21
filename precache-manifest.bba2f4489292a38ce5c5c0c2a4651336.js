6a93899c5b22c71871cb0594ca6761834c840d8b
RYOTA <kobaryo04ysh@icloud.com>
2020-10-21 16:48:33 +0900

    Updates

---
+++ precache-manifest.bba2f4489292a38ce5c5c0c2a4651336.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f98e90ec896d4268462ce80205012e66",
+    "url": "/todo-app1/index.html"
+  },
+  {
+    "revision": "135ac013abe399ad48f7",
+    "url": "/todo-app1/static/css/main.3b5933b4.chunk.css"
+  },
+  {
+    "revision": "37eb3f3146dde8edd550",
+    "url": "/todo-app1/static/js/2.cb7c3f46.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/todo-app1/static/js/2.cb7c3f46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "135ac013abe399ad48f7",
+    "url": "/todo-app1/static/js/main.1a7a2d10.chunk.js"
+  },
+  {
+    "revision": "494c8769d756030cad6c",
+    "url": "/todo-app1/static/js/runtime-main.ed1d49fb.js"
+  }
+]);
