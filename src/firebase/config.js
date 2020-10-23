01ac3bc1e9c8df1efe9e3df7b9d22e6233c27277
RYOTA <kobaryo04ysh@icloud.com>
2020-10-23 21:52:40 +0900

    deploy app on firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBunyk4Q1xL2qzZRgxVvm7zhwFv-HX8R88",
+    authDomain: "todo-app-a478e.firebaseapp.com",
+    databaseURL: "https://todo-app-a478e.firebaseio.com",
+    projectId: "todo-app-a478e",
+    storageBucket: "todo-app-a478e.appspot.com",
+    messagingSenderId: "470319899512",
+    appId: "1:470319899512:web:4a232eddf661163aaa5e8d"
+};
